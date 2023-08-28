e8f8975b49d85db557d35a6dcb5f43b89719230a
Arkadiusz Myszka <lokonhinio@gmail.com>
2023-08-29 01:01:55 +0200

    prepare

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,59 @@
+import { useState, useEffect } from 'react';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+import { nanoid } from 'nanoid';
+import css from './App.module.css';
+
 export const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    const storedContacts = JSON.parse(localStorage.getItem('contacts'));
+    if (storedContacts) {
+      setContacts(storedContacts);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const findContacts = () => {
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
+  const deleteContact = id => {
+    setContacts(contacts.filter(contact => contact.id !== id));
+    setFilter('');
+  };
+  const formSubmit = (name, number) => {
+    const contact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+
+    contacts.some(
+      i => i.name.toLowerCase() === name.toLowerCase() || i.number === number
+    )
+      ? alert(`${name} is already in contacts`)
+      : setContacts([contact, ...contacts]);
+  };
+
+  const changeFilterInput = (e) => {
+    setFilter(e.target.value );
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <section>
+      <h1 className={css.title}>Phonebook</h1>
+      <ContactForm onSubmit={formSubmit} />
+      <h2>Contacts</h2>
+      <Filter filter={filter} changeFilterInput={changeFilterInput} />
+      <ContactList contacts={findContacts()} deleteContact={deleteContact} />
+    </section>
   );
 };
